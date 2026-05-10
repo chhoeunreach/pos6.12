ee39348d36321f4a7ba4bf02f2e5b0ae825976ee
chhoeunreach <chhoeunreach@gmail.com>
2026-05-10 10:08:29 +0700

    inventory  100525 10AM

--- Modules/SmartStockInventory/Resources/views/dashboard/index.blade.php
+++ Modules/SmartStockInventory/Resources/views/dashboard/index.blade.php
@@ -7,12 +7,10 @@
 <input type="hidden" name="start_date" id="ssi_start_date" value="{{ $filters['start_date'] ?? '' }}">
 <input type="hidden" name="end_date" id="ssi_end_date" value="{{ $filters['end_date'] ?? '' }}">
 <div class="col-md-3">
-    <select class="form-control select2" name="location_id" id="dashboard_location">
-        <option value="">{{ __('lang_v1.select_location') }}</option>
-        @foreach($locations as $loc)
-            <option value="{{ $loc->id }}" {{ in_array($loc->id, $locationIds ?? []) ? 'selected' : '' }}>{{ $loc->name }}</option>
-        @endforeach
-    </select>
+    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#ssiLocationFilterModal">
+        <i class="fa fa-map-marker"></i> Select Locations
+    </button>
+    <small class="help-block">Selected: <span id="ssi_selected_locations_count">{{ count((array)($locationIds ?? [])) }}</span></small>
 </div>
 <div class="col-md-3">
     <button type="button" id="dashboard_date_filter" class="btn btn-default">
@@ -23,6 +21,28 @@
 <div class="col-md-4 text-right"><a class="btn btn-success" href="{{ route('ssi.dashboard.export', request()->all()) }}">Export Dashboard</a> <a class="btn btn-default" href="{{ route('ssi.dashboard.print', request()->all()) }}">Print Summary</a></div>
 </form>
 </div></div>
+
+<div class="modal fade" id="ssiLocationFilterModal" tabindex="-1" role="dialog" aria-labelledby="ssiLocationFilterModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="ssiLocationFilterModalLabel">Select Business Locations</h4>
+            </div>
+            <div class="modal-body">
+                <select class="form-control select2" id="dashboard_location_modal" multiple style="width:100%;">
+                    @foreach($locations as $loc)
+                        <option value="{{ $loc->id }}" {{ in_array((int)$loc->id, (array)($locationIds ?? []), true) ? 'selected' : '' }}>{{ $loc->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="ssi_apply_location_filter">Apply</button>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row">
     @foreach([
         ['label' => 'Total Products', 'value' => $totalProducts, 'metric' => 'total_products'],
@@ -60,7 +80,17 @@ $(function(){
     }
     $('#dashboard_date_filter').daterangepicker(dateRangeSettings, function(s,e){ setDates(s,e); });
     setDates(start,end);
-    $('#dashboard_location').on('change', function(){ $('#ssi_dashboard_filter_form').submit(); });
+    $('#dashboard_location_modal').select2({ width:'100%', dropdownParent: $('#ssiLocationFilterModal') });
+    $('#ssi_apply_location_filter').on('click', function(){
+        $('#ssi_dashboard_filter_form input[name="location_ids[]"]').remove();
+        var vals = $('#dashboard_location_modal').val() || [];
+        vals.forEach(function(v){
+            $('#ssi_dashboard_filter_form').append('<input type="hidden" name="location_ids[]" value="'+v+'">');
+        });
+        $('#ssi_selected_locations_count').text(vals.length);
+        $('#ssiLocationFilterModal').modal('hide');
+        $('#ssi_dashboard_filter_form').submit();
+    });
 });
 </script>
 @endsection
