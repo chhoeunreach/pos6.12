f7007ea10830f2d24a76baeb0840d153eb8b4a00
chhoeunreach <chhoeunreach@gmail.com>
2026-07-01 18:57:49 +0700

    export4

--- app/Imports/BlankImport.php
+++ app/Imports/BlankImport.php
@@ -3,8 +3,9 @@
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\ToArray;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class BlankImport implements ToArray
+class BlankImport implements ToArray, WithHeadingRow
 {
     public function array(array $array)
     {
