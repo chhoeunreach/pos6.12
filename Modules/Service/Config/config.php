3253e584df97ae169ff962a75ffd5ec60d4d1b66
chhoeunreach <chhoeunreach@gmail.com>
2026-07-02 11:36:11 +0700

    update service

--- Modules/Service/Config/config.php
+++ Modules/Service/Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name' => 'Service',
     'module_version' => '1.0.0',
-    'route_prefix' => env('SERVICE_ROUTE_PREFIX', 'service'),
+    'route_prefix' => env('SERVICE_ROUTE_PREFIX', 'pos/service'),
     'route_name_prefix' => 'service.',
     'database_connection' => env('SERVICE_DB_CONNECTION', 'service'),
     'excluded_main_modules' => [],
