8ae90f242273ba389893a74c7e60dfa42afbd97a
chhoeunreach <chhoeunreach@gmail.com>
2026-07-03 11:55:21 +0700

    update197

--- resources/views/report/stock_transfer_report.blade.php
+++ resources/views/report/stock_transfer_report.blade.php
@@ -101,6 +101,7 @@
                                 <th></th>
                                 <th></th>
                                 <th></th>
+                                <th></th>
                             </tr>
                         </tfoot>
                     </table>
