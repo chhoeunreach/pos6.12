8d0394d68be97783db69d9d0567b95b49c7f6592
chhoeunreach <chhoeunreach@gmail.com>
2026-07-21 23:56:15 +0700

    Recalculate edit schedule amounts during schedule refresh

--- Modules/LoanManagement/Http/Controllers/LoanInstallmentListController.php
+++ Modules/LoanManagement/Http/Controllers/LoanInstallmentListController.php
@@ -2899,6 +2899,7 @@ class LoanInstallmentListController extends Controller
             abort_if(! $loanRow, 404);
 
             $data = $this->scheduleRefreshDataFromLoan($loan, $loanRow);
+            $data = $this->recalculateEditScheduleAmounts($loan, $loanRow, $data);
 
             DB::connection('mysql_loan')->transaction(function () use ($loan, $loanRow, $data) {
                 $meta = ! empty($loanRow->meta_json) ? (json_decode((string) $loanRow->meta_json, true) ?: []) : [];
