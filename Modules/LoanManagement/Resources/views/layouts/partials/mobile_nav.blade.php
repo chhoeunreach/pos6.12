ba94ff49c52c5396cb024c9139686e5d03848ce3
chhoeunreach <chhoeunreach@gmail.com>
2026-09-10 10:17:19 +0700

    tt

--- Modules/LoanManagement/Resources/views/layouts/partials/mobile_nav.blade.php
+++ Modules/LoanManagement/Resources/views/layouts/partials/mobile_nav.blade.php
@@ -4,6 +4,7 @@
     $overdueCount = (int) ($mobileNavBadges['overdue'] ?? 0);
 
     $currentRoute = optional(request()->route())->getName() ?? '';
+    $canViewChats = \Modules\LoanManagement\Helpers\LoanMenuHelper::loanUserCan('loan_management.chat.view');
 @endphp
 
 <nav class="lm-mobile-nav d-lg-none" id="loanMobileNav">
@@ -21,6 +22,16 @@
         <i class="fa fa-money"></i>
         <span>Collection</span>
     </a>
+
+    @if($canViewChats)
+        <a href="{{ route('loan-management.chat.index') }}" class="{{ str_starts_with($currentRoute, 'loan-management.chat') || str_starts_with($currentRoute, 'loan-management.live-chat') ? 'active' : '' }}">
+            <i class="fa fa-telegram"></i>
+            <span>Chats</span>
+            @if($unreadChatCount > 0)
+                <em class="lm-mobile-nav-badge">{{ $unreadChatCount > 99 ? '99+' : $unreadChatCount }}</em>
+            @endif
+        </a>
+    @endif
 
     <button type="button" id="loanMobileSidebarToggle" aria-label="Open full menu">
         <i class="fa fa-users"></i>
